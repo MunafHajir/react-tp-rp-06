c4f025c01b41808ba3b4dfda5e194d34e3cf46a8
Munaf Hajir <munafhajirpro@gmail.com>
2024-06-03 21:07:02 +0530

    Added Event Handling and State code

--- learning/src/components/Login.js
+++ learning/src/components/Login.js
@@ -1,24 +1,53 @@
-import Input from '../common/Input';
-import Button from '../common/Button'
+import { useState } from "react";
 
 function Login() {
     var componentName = "Login";
+    var [username, setUsername] = useState("")
+    var [password, setPassword] = useState("")
+
+    function handleUserChange(event) {
+        var value = event.target.value;
+        setUsername(value)
+
+    }
+
+    function handlePasswordChange(event) {
+        var value = event.target.value;
+        setPassword(value)
+    }
 
     return (
-        <div style={{backgroundColor: "blue", color: "white"}}>
+        <>
+        <div style={{textAlign: "center"}}>
             <h1>{componentName}</h1>
 
-            <div>
-                <Input />
+            <div style={{padding: "20px"}}>
+                <input 
+                onChange={ function(event) { handleUserChange(event) }  }
+                type="text" 
+                placeholder="Enter Username" 
+                style={{padding: "10px"}}/>
             </div>
-            <div>
-                <Input />
+            <div style={{padding: "20px"}}>
+                <input 
+                onChange={ function(event) {
+                    handlePasswordChange(event)
+                } }
+                type="password" 
+                placeholder="Enter Password" 
+                style={{padding: "10px"}}/>
             </div>
 
             <div>
-                <Button />
+                <button style={{padding: "10px 20px"}}> Submit </button>
             </div>
         </div>
+
+        <div>
+              <h1>Username: { username  }</h1>  
+              <h2>Password: { password } </h2>
+        </div>
+        </>
 
     )
 }
